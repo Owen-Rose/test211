c9c9edbb03de2dd8cda1dcbb21b82ab3707c00ee
Owen-Rose <owen.rose@edu.sait.ca>
2023-04-19 11:41:58 -0600

    first commit

---
+++ DatabaseManagement.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+using System.Data;
+using System.Security.Cryptography;
+
+namespace test211_console
+{
+    public class DatabaseManagement
+    {
+        private MySqlConnection _connection;
+        private string _server;
+        private string _database;
+        private string _uid;
+        private string _password;
+        private int _port;
+        public DatabaseManagement() 
+        {
+            _server = "localhost";
+            _database = "test211";
+            _uid = "root";
+            _port = 3306;
+            _password = "admin";
+            string connectionString = $"SERVER={_server};DATABASE={_database};UID={_uid};PORT = {_port};PASSWORD={_password};";
+            _connection = new MySqlConnection(connectionString);
+
+        }
+
+        public void OpenConnction()
+        {
+            if (_connection.State != ConnectionState.Open)
+            {
+                _connection.Open();
+            }
+        }
+
+        public void CloseConnction()
+        {
+            if ( _connection.State != ConnectionState.Closed)
+            { 
+                _connection.Close(); 
+            }
+        }
+
+        public DataTable GetUsers()
+        {
+            OpenConnction();
+            string query = "SELECT * FROM users";
+            MySqlCommand command = new MySqlCommand(query, _connection);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            CloseConnction();
+            return table;
+        }
+        public void CreateUser(string name, string email)
+        {
+            OpenConnction();
+            string query = "INSERT INTO users (name, email) VALUES (@name, @email)";
+            MySqlCommand command = new MySqlCommand(query, _connection);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@email", email);
+            command.ExecuteNonQuery();
+            CloseConnction();
+        } 
+        
+        public void UpdateUser(string name, string email, int userId)
+        {
+            OpenConnction();
+            string query = "UPDATE users SET name = @name, email = @email WHERE id = @id";
+            MySqlCommand command = new MySqlCommand(query, _connection);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@email", email);
+            command.Parameters.AddWithValue("@id", userId);
+            command.ExecuteNonQuery();
+            CloseConnction();
+        }
+
+
+        public void DeleteUser(int id) 
+        {
+            OpenConnction();
+            string query = "DELETE FROM users WHERE id = @id";
+            MySqlCommand command = new MySqlCommand(query, _connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.ExecuteNonQuery();
+        }
+
+
+      
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using MySqlConnector;
+using System.Data;
+using System.ComponentModel.Design;
+using test211_console;
+
+public class Program
+{
+    public static void Main()
+    {
+        DatabaseManagement databaseManagement = new DatabaseManagement();
+        Console.WriteLine("Hello friend.");
+
+        Console.WriteLine("Please select an option from the menu:\n\n" +
+            "1. Create new User.\n\n" +
+            "2. Update user information\n\n" +
+            "3. Delete User\n\n" +
+            "4. Display list of users.");
+        string userInput = Console.ReadLine();
+        switch (userInput)
+        {
+            case "1":
+                Console.WriteLine("please input the new user's information.\n\nname:");
+                string name = Console.ReadLine();
+                Console.WriteLine("email: ");
+                string email = Console.ReadLine();
+                databaseManagement.CreateUser(name, email);
+                break;
+            case "2":
+                Console.WriteLine("Please enter the user Id of the user who's information you'd like to update, along with their updated inforamtion.");
+                Console.WriteLine();
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                name = Console.ReadLine();
+                Console.WriteLine();
+                email = Console.ReadLine();
+                databaseManagement.UpdateUser(name, email, id);
+
+                break;
+            case "3":
+                Console.WriteLine("Please enter the id of the User you'd like to remove from the system.\n\nuser ID: ");
+                id = Convert.ToInt32(Console.ReadLine());
+                databaseManagement.DeleteUser(id);
+                break;
+            case "4":
+                DataTable users = databaseManagement.GetUsers();
+                foreach (DataRow row in users.Rows)
+                {
+                    id = (int)row["id"];
+                    name = (string)row["name"];
+                    email = (string)row["email"];
+                    Console.WriteLine($"ID: {id}, Name: {name}, Email: {email}");
+                }
+
+                break;
+        }
+    }
+
+}
